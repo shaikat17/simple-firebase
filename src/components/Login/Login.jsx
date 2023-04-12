e0e1cd9fc7ac099aacac1ff360ac39b87b3ac009
shaikat17 <shaikatpal56@gmail.com>
2023-04-12 22:57:55 +0600

    logout is also working

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react'
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app);
     console.log(app)
     const provider = new GoogleAuthProvider();
@@ -12,15 +13,34 @@ const Login = () => {
         .then(result => {
             const user = result.user
             console.log(user)
+            setUser(user)
         })
         .catch( error => {
             const errorMessage = error.message
             console.log(error.msg)
         })
     }
+
+    const handleSignOut = () => {
+        signOut(auth)
+        .then(result => {
+            console.log(result)
+            setUser(null)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
   return (
     <div>
+        { user ? <button onClick={handleSignOut}>Logout</button> :
         <button onClick={handleGoogleSignIn}>Google Login</button>
+        }
+        { user && <div>
+            <h3>{user.displayName}</h3>
+            <p>{user.email}</p>
+            <img src={user.photoURL} alt="" />
+        </div>}
     </div>
   )
 }
